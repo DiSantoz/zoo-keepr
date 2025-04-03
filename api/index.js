d5d20f3df62e9cb2da6d8b1d1e33b7679d90b449
DiSantoz <dferreira0191@gmail.com>
2025-04-03 18:36:18 -0400

    added zookeepers, corrected errors

--- api/index.js
+++ api/index.js
@@ -1,21 +1,18 @@
 const apiRoutes = require("../routes/apiRoutes");
 const htmlRoutes = require("../routes/htmlRoutes");
 const express = require("express");
-const { animals } = require("../data/animals.json");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static("public"));
-const fs = require("fs");
-const path = require("path");
+
+// middleware
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // PORT
 const PORT = 3001;
-
-// *********************************** ROUTES ***********************************************
 
 // listen for port
 app.listen(PORT, () => {
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,22 +3,22 @@ const router = require("express").Router();
 
 // homepage
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 // animals page
 router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/animals.html"));
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
 // zookeeper page
 router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/zookeepers.html"));
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
 // wildcard route
 router.get("*wildcard", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
