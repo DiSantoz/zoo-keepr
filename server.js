f82ed2001a8951c556fca2ae1e023fa0409f82a5
DiSantoz <dferreira0191@gmail.com>
2025-04-02 20:35:12 -0400

    added filtering and changed port

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 const express = require("express");
 const { animals } = require("./data/animals.json");
 const app = express();
+const PORT = process.env.PORT || 3001;
+// filter query
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+
+    personalityTraitsArray.forEach((trait) => {
+      filteredResults = filteredResults.filter(
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+  // return the filtered results:
+  return filteredResults;
+}
 
 // fetch json data
 app.get("/api/animals", (req, res) => {
-  res.json(animals);
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
 
 // listen for port
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`server is running at http://localhost:3001/api/animals`);
 });
